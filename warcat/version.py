41566d9db68a20946dedd14e7c5d049f9931d889
Christopher Foo <chris.foo@gmail.com>
2014-05-27 18:18:20 -0400

    Bumps version to 2.3a1

--- warcat/version.py
+++ warcat/version.py
@@ -1,9 +1,9 @@
 '''Version info'''
 
-short_version = '2.2'
+short_version = '2.3'
 '''Short version in the form of N.N'''
 
-__version__ = short_version + '.2'
+__version__ = short_version + 'a1'
 '''Version in the form of N.N[.N]+[{a|b|c|rc}N[.N]+][.postN][.devN]'''
 
 
